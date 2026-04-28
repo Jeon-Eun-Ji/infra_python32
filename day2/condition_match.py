07e687180c53d3fd986b66e7785e8cc68309273f
Jeon-Eun-Ji <2022304064@skuniv.ac.kr>
2026-04-28 11:43:08 +0900

    조건문 match case

---
+++ day2/condition_match.py
@@ -0,0 +1,9 @@
+log_data = ["LOGIN", "jeoneunji", "127.0.0.1"]
+
+match log_data:
+    case ["LOGIN", user, ip]:
+        print(f"{user}가 {ip}에서 로그인했었네?")
+    case ["LOGOUT", user]:
+        print(f"{user}가 나갔네?")
+    case _:
+        print(f"대체 뭐지..? {log_data}")
