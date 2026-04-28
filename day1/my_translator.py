22851432cc97c666768e1cc719befb5037883bd2
Jeon-Eun-Ji <2022304064@skuniv.ac.kr>
2026-04-28 10:25:22 +0900

    day1 테스트

--- day1/my_translator.py
+++ day1/my_translator.py
@@ -1,6 +1,9 @@
 from deep_translator import GoogleTranslator
 
-my_translator = GoogleTranslator(source="ko", target="en")
+translator_list = [GoogleTranslator(source="ko", target="en"), "국어 점수를 입력하세요."]
+print(translator_list[0].translate(translator_list))
 
-result = my_translator.translate("안녕하세요. 저는 전은지입니다.")
-print(result)
+
+#my_translator = GoogleTranslator(source="ko", target="en")
+#result = my_translator.translate("안녕하세요. 저는 전은지입니다.")
+#p#rint(result)
